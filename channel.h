15b11adfa030ced7c0b45da46cab995e1c3d5b7d
David Case <Dav3xor@gmail.com>
2018-06-06 17:27:54 -0700

    better frequency setting

--- channel.h
+++ channel.h
@@ -4,14 +4,23 @@
 
 class Channel {
   public:
-    void set_frequency(uint8_t freq) {
+    void set_frequency(uint16_t freq) {
       frequency = freq;
+      note_info &= 0xfc;
+      note_info |= (frequency >> 8);
     }
+
     void set_note_on(void) {
       note_info |= CH_NOTE_ON;
     }
+
     void set_note_off(void) {
       note_info &= ~(CH_NOTE_ON);
+    }
+
+    void set_block_number(uint8_t block) {
+      note_info &= 0x1c; // abcdef
+      note_info |= (block << 2)
     }
 
   private:
