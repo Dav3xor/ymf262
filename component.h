501af03d56e5c0d3b86c0041b8494071e2fb280a
David Case <Dav3xor@gmail.com>
2018-06-10 11:52:22 -0700

    started on data io functions, added file for holding channel/operator maps

--- component.h
+++ component.h
@@ -1,14 +1,19 @@
 class Component {
   public:
-    void set_register(uint8_t reg) {
+    void set_register(uint8_t reg, uint8_t array=0) {
       // set the current register
-      if(reg != cur_register) {
-        // set here
-        cur_register = reg;
-      }
+      // 0. cs = low
+      // 1. rd = high
+      // 2. wr = low
+      // 3. a0 = low
+      // 4. a1 = array (2nd argument) 
     }
     void set_data(uint8_t data) {
       // set the data for the given register
+      // 0. cs = low
+      // 1. rd = high
+      // 2. wr = low
+      // 3. a0 = high
     }
     void set_flag(uint8_t flag) {
       flags |= flag;
@@ -19,5 +24,4 @@ class Component {
     }
   protected:
     uint8_t flags;
-    uint8_t cur_register;
 };
